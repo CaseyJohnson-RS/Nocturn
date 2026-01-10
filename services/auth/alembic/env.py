cd6a4776e04e2915dbe11e6f987b8f7693100637
Shamukhametov Ruslan <sharus.programmer@gmail.com>
2026-01-11 00:09:42 +0700

    add: initial table migration

--- services/auth/alembic/env.py
+++ services/auth/alembic/env.py
@@ -16,14 +16,32 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.models import ( # noqa: F401, E402
+    Base,
+    RefreshToken,
+    EmailVerificationToken,
+    PasswordResetToken,
+    SecurityEvent,
+    User,
+)  
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv(f".env.{os.getenv('ENV', '')}")
+
+from app.core.settings import settings  # noqa: E402
+
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.database_url_sync,
+)
 
 
 def run_migrations_offline() -> None:
