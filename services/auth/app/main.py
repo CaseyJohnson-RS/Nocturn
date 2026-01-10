07411acf90e7da245a8518314ce8ec0e75bb0fb6
Shamukhametov Ruslan <sharus.programmer@gmail.com>
2026-01-10 12:47:45 +0700

    fix: what's with branch responsibility?

--- services/auth/app/main.py
+++ services/auth/app/main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from auth!")
+from fastapi import FastAPI  
 
+app = FastAPI()  
 
-if __name__ == "__main__":
-    main()
+@app.get("/")  
+async def index():  
+    return {"status": "It's ALIVE!"}
