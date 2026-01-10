34203d34d7bb9a00d6e1eec3481a1ec2281e637d
Shamukhametov Ruslan <sharus.programmer@gmail.com>
2026-01-10 22:54:51 +0700

    update: new databases for dev, test and local hosting

--- infra/postgres/init/02_create_dbs.sh
+++ infra/postgres/init/02_create_dbs.sh
@@ -2,6 +2,11 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE DATABASE auth OWNER auth_user;
-  CREATE DATABASE notes OWNER notes_user;
+  CREATE DATABASE auth_local OWNER auth_user;
+  CREATE DATABASE auth_dev OWNER auth_user;
+  CREATE DATABASE auth_test OWNER auth_user;
+  
+  CREATE DATABASE notes_local OWNER notes_user;
+  CREATE DATABASE notes_dev OWNER notes_user;
+  CREATE DATABASE notes_test OWNER notes_user;
 EOSQL
