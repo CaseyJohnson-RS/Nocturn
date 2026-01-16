a9423fcef78061a298fff4c94b4e46d3355dee98
Shamukhametov Ruslan <sharus.programmer@gmail.com>
2026-01-16 14:49:17 +0700

    add: EmailOutbox table to package

--- services/auth/app/models/__init__.py
+++ services/auth/app/models/__init__.py
@@ -4,3 +4,4 @@ from .security_event import SecurityEvent # noqa: F401
 from .password_reset_token import PasswordResetToken # noqa: F401
 from .email_verification_token import EmailVerificationToken # noqa: F401
 from .refresh_token import RefreshToken # noqa: F401
+from .email_outbox import EmailOutbox # noqa: F401
