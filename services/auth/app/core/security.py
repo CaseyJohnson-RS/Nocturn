c1bc645a2d065a1339d714320cb5ac3a1ca6698e
Shamukhametov Ruslan <sharus.programmer@gmail.com>
2026-01-18 22:32:46 +0700

    udpate: simplify email token generation

--- services/auth/app/core/security.py
+++ services/auth/app/core/security.py
@@ -1,6 +1,5 @@
 import hashlib
 import secrets
-from time import time # type: ignore[attr-defined]
 
 from passlib.context import CryptContext
 
@@ -15,9 +14,7 @@ def verify_password(password: str, hashed: str) -> bool:
     return pwd_context.verify(password, hashed)
 
 def generate_token(length: int = 32) -> str:
-    random_part = secrets.token_urlsafe(length)
-    timestamp = str(int(time() * 1000))  # миллисекунды
-    token = f"{random_part}-{timestamp}"
+    token = secrets.token_urlsafe(length)
     return token
 
 def hash_token(token: str) -> str:
